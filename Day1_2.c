1cd957374761dbb3060473a042df239ce949cdfb
Ani-svg <aniket.shishodia1@gmail.com>
2026-02-01 23:04:31 +0530

    Update Day1_2.c

--- Day1_2.c
+++ Day1_2.c
@@ -4,18 +4,18 @@ int main() {
     int n, target;
     int nums[100];
 
-    // Read number of elements
+    
     scanf("%d", &n);
 
-    // Read array elements
+    
     for (int i = 0; i < n; i++) {
         scanf("%d", &nums[i]);
     }
 
-    // Read target
+    
     scanf("%d", &target);
 
-    // Find two indices
+    
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (nums[i] + nums[j] == target) {
@@ -27,3 +27,4 @@ int main() {
 
     return 0;
 }
+
