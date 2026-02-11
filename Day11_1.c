6f5df158dadfa982a7ec1072c669efd95125de49
Ani-svg <aniket.shishodia1@gmail.com>
2026-02-11 20:05:33 +0530

    Add files via upload

---
+++ Day11_1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int A[10][10], B[10][10], C[10][10];
+
+    // Read dimensions
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    // Read first matrix
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    // Read second matrix
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Add matrices
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    // Display result
+    printf("Sum of the matrices:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
